6e4d3af29c0d97e16cca590759cf38ba60e840c5
dowzwi <akkfaer3@gmail.com>
2024-10-25 19:06:52 +0600

    geeks4

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('http_response/',views.http_response),
+path('http_response/',views.html_response),
+]
